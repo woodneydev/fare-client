70300909149406dcb2603aa834bb337037181882
Woodney Juste <woodneydev@gmail.com>
2023-08-30 01:47:45 -0400

    landing and login structure add, styling in progress

--- src/components/LoginHero/LoginHero.js
+++ src/components/LoginHero/LoginHero.js
@@ -1,12 +1,16 @@
 import "./LoginHero.scss";
-import drivingConvertible from "../../../src/assets/videos/drivingConvertible.mp4"
-
+import drivingConvertible from "../../../src/assets/videos/drivingConvertible.webm"
+import hamburger from "../../assets/icons/hamburger.png"
 function LoginHero() {
 
     return (
-        <section className="hero">      
-            <h2 className="hero__header" >Fare!</h2>
-            <video className="hero__video" src={drivingConvertible} type="video/mp4"  autoPlay="autoplay" loop muted>
+        <section className="hero">
+            <div className="hero__nav">
+                <h2 className="hero__header" >Fare!</h2>
+                <img className="hero__img" src={hamburger} alt="hamburger menu" />
+            </div>      
+            
+            <video className="hero__video" src={drivingConvertible} type="video/webm"  autoPlay="autoplay" loop muted>
             </video>
         </section>
     )
--- src/pages/HomePage.js/HomePage.js
+++ src/pages/HomePage.js/HomePage.js
@@ -1,5 +1,6 @@
 import "./HomePage.scss";
 import videobackground from "../../assets/videos/home-background.webm";
+import hamburger from "../../assets/icons/hamburger.png";
 
 function HomePage() {
 
@@ -12,19 +13,39 @@ function HomePage() {
             <section className="content">
 
                 <div className="content__box content__box--nav">
-                    <h2>title</h2>
+                    <nav className="nav">
+                        <div className="nav__section nav__section--start">
+                          <h2 className="nav__start-title">Fare</h2>  
+                        </div>
+                        
+                        <ul className="nav__section nav__section--mid">
+                            <li className="nav__section-item">Home</li>
+                            <li className="nav__section-item">Driver</li>
+                            <li className="nav__section-item">Rider</li>
+                            <li className="nav__section-item">About Us</li>
+                        </ul>
+
+                        <ul className="nav__section nav__section--end">
+                            <li className="nav__section-item">Login</li>
+                            <li className="nav__section-item">Register</li>
+                        </ul>
+                        <img className="nav__img" src={hamburger} alt="hamburger menu" />
+                    </nav>
                 </div>
 
                 <div className="content__box content__box--info">
-                    <h2>title</h2>
+                    <section className="headline">
+                        <h1 className="headline__title">Rideshare That's Fair For the Rider and the Driver</h1>
+                        <p className="headline__description">Discover never seen before rideshare features including: rider price filters, driver wage setting, calendar scheduling, and more.</p>
+                    </section>
                 </div>
 
-                <div className="content__box content__box--interact">
-                    <div className="advance">
+                <form className="content__box content__box--interact">
+                    <label className="advance">
                         <p className="advance__text">Find a ride</p>
                         <button className="advance__button">{"-->"}</button>
-                    </div>
-                </div>
+                    </label>
+                </form>
 
             </section>
         
